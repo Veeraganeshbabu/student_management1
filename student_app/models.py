78989585e95a3c40cbaeca1260e489db89477281
Veera ganesh babu <99416934+Veeraganeshbabu@users.noreply.github.com>
2022-07-22 12:30:00 +0530

    Update models.py

--- student_app/models.py
+++ student_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Details(models.Model):
+    StudentId = models.CharField(max_length=200, null=True)
+    StudentName = models.CharField(max_length=50, null=True)
+    BatchName = models.CharField(max_length=50, null=True)
+
+    Graduation = models.CharField(max_length=100, null=True)
+    YearOfPass = models.IntegerField(null=True)
+    EMail = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.StudentName
+
+
+
